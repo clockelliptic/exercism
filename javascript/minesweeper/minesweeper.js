4249c3c8c291f44e75cf7dc9cf27f98d8c2032bf
clockelliptic <allenmichaelhart@gmail.com>
2019-09-28 13:59:36 -0400

    updated minesweeper

--- javascript/minesweeper/minesweeper.js
+++ javascript/minesweeper/minesweeper.js
@@ -1,17 +1,27 @@
-export function annotate (field) {
-	//map over rows
-	return field.map((row, j, field_arr) => {
-		//map over characters in each row
-		return row.split('').map((cell_value, i) => {
-			//count mines adjacent to each char
-			const sweep_zone = field_arr
-				.slice((j<=0) ? 0 : j-1, j+2) 
-				.map(row => row.substring(i-1, i+2));
-			const mines = sweep_zone.join('').match(/[*]/g);
-			//annotate each cell in the minefield
-			return (cell_value==MINE) ? MINE : (mines==null) ? ' ' : mines.length.toString()
-		}).join('')
-	});
+export const annotate = (minefield) => {
+	const grid = minefield.map(r => [...r].map(c => (c===MINE) ? MINE : 0))
+	const mines = grid.flatMap((r, j) => r		
+			.map((c, i) => (c==MINE) ? [i, j] : ' ')
+			.filter(x => x !== ' '));
+	const annotated_grid = mines
+		.reduce(count_mine, grid)
+		.map(r => r.map(c => (c==0) ? ' ' : c.toString()).join(''))
+	return annotated_grid
 }
 
-const MINE = '*'
+const count_mine = (acc_arr, mine_pos) =>
+	acc_arr.map((r, j) => 
+		r.map((c, i) => 
+			(c!==MINE && adjacent_to_mine([i, j], mine_pos))
+				? c += 1
+				: c
+		)
+	);
+
+const adjacent_to_mine = (curr_pos, mine_pos) => 
+	((curr_pos[0]-mine_pos[0])**2 + (curr_pos[1]-mine_pos[1])**2) <= 2
+
+const MINE = '*';
+
+//const field0 = ["  *", " * ", "*  "]
+//annotate(field0);
